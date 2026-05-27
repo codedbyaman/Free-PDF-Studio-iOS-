deb71b0b7d887fc027fc33b900aa6e744ddaad83
aman kumar <aman.kumar@kroger.com>
2026-05-28 01:34:25 +0530

    Initial Commit

---
+++ Free PDF Studio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Free PDF Studio
+//
+//  Created by Aman kumar on 28/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Free PDF Studio/Free_PDF_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Free_PDF_StudioApp.swift
+//  Free PDF Studio
+//
+//  Created by Aman kumar on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Free_PDF_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
